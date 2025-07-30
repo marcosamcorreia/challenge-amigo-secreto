287c0786ae17d9eda36850031c5f386580dc3470
Marcos Alexandre <mmarcos.correiaa@gmail.com>
2025-07-30 16:44:14 -0300

    Adicionar funcoes ao codigo

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = []
+let amigo = document.querySelector("#amigo")  //Nome digitado
+let campoDeAmigos = document.querySelector("#listaAmigos") //Campo de texto
+let campoResultado = document.querySelector("#resultado") //Campo de resultado
+
+function adicionarAmigo(){
+    if (amigo.value==""){
+        alert("Digite um nome.")
+
+    } else{
+            campoDeAmigos.innerHTML+=`<p>${amigo.value}</p>`
+            listaDeAmigos.push(amigo.value)       
+    }
+    amigo.value=""
+}
+
+function sortearAmigo(){
+    let max= listaDeAmigos.length
+    if (max==0){
+        alert("Adicione um nome primeiro.")
+    }else if(max==1){
+        alert("Adicione mais um nome")
+    }else{
+        let sorteia=parseInt(Math.random()*max)
+        campoResultado.innerHTML=listaDeAmigos[sorteia]
+
+    }
+}
